dced2aba31a61da30374b88b5ab3a4d3fb787394
Horácio Macêdo <horacio97@gmail.com>
2019-08-06 19:24:22 -0300

    literalmente o projeto

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public Transform focusOn;
+    public Vector3 cameraOffset;
+    public float minZoom = 4f;
+    public float zoomSpeed = 5f;
+    public float maxZoom = 12f;
+    public float minPitch = 0f;
+    public float pitch = 2f;
+    public float pitchDelta = 0.2f;
+    public float maxPitch = 4f;
+    public float yawSpeed = 100f;
+
+    private float CurrentZoom = 10f;
+    private float CurrentYaw = 0f;
+    private float CurrentPitch = 2f;
+
+    void Update(){
+        CurrentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        CurrentZoom = Mathf.Clamp(CurrentZoom, minZoom, maxZoom); 
+        CurrentYaw -= Input.GetAxis("Horizontal")*yawSpeed*Time.deltaTime;
+        if(Input.GetAxis("Mouse ScrollWheel") > 0f) CurrentPitch -= CurrentPitch*pitchDelta;
+        if(Input.GetAxis("Mouse ScrollWheel") < 0f) CurrentPitch += CurrentPitch*pitchDelta;
+        CurrentPitch = Mathf.Clamp(CurrentPitch, minPitch, maxPitch);
+    }
+
+    void LateUpdate(){
+        transform.position = focusOn.position - cameraOffset * CurrentZoom;
+        transform.LookAt(focusOn.position + Vector3.up * CurrentPitch);
+        transform.RotateAround(focusOn.position, Vector3.up, CurrentYaw);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask moveMask;
+    PlayerMotor playerMotor;
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start(){
+        playerMotor = GetComponent<PlayerMotor>();
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1)){
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if(Physics.Raycast(ray, out hit, 100, moveMask)){
+                Debug.Log("We hit " + hit.collider.name + " " + hit.point);
+                playerMotor.MoveToPoint(hit.point);
+            }
+            if(Input.GetMouseButtonDown(1)){
+                if(Physics.Raycast(ray,out hit)){
+
+                }
+            }
+        }
+    }
+}
